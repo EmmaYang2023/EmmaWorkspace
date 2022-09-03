971bc7e7cceccf40dd084a148a5f4420493305d6
Arnold Fifield1 <arnoldfifield@Arnolds-MBP.fios-router.home>
2022-09-03 12:16:24 -0400

    add education revision two

--- src/test/java/uiStepDefinitions/AddEducation.java
+++ src/test/java/uiStepDefinitions/AddEducation.java
@@ -1,14 +1,11 @@
 package uiStepDefinitions;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
 import java.util.Map;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 
-import io.cucumber.java.en.And;
+import io.cucumber.datatable.DataTable;
 import io.cucumber.java.en.Then;
 import utilities.DriverFactory;
 
@@ -17,7 +14,7 @@ public class AddEducation {
 	private WebDriver driver = DriverFactory.getInstance();
 
 	@Then("User updates {string}")
-	public void user_updates(String string, io.cucumber.datatable.DataTable dataTable) throws InterruptedException {
+	public void user_updates_add_education(DataTable dataTable) throws InterruptedException {
 		Map<String, String> addEducation = dataTable.asMaps().get(0);
 		driver.findElement(By.linkText("Add Education")).click();
 		driver.findElement(By.name("school")).sendKeys(addEducation.get("school"));
