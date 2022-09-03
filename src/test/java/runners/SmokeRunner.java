1782b32b67da1a3b466052c3697c53d163b77500
Muradil Erkin <miller.muradil@gmail.com>
2022-09-03 09:44:41 -0400

    clean up porject

--- src/test/java/runners/SmokeRunner.java
+++ src/test/java/runners/SmokeRunner.java
@@ -1,4 +1,4 @@
-package hellocucumber;
+package runners;
 
 import org.junit.platform.suite.api.ConfigurationParameter;
 import org.junit.platform.suite.api.IncludeEngines;
@@ -13,6 +13,6 @@ import static io.cucumber.junit.platform.engine.Constants.*;
 @ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty, html:target/html-report/index.html")
 @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "uiStepDefinitions, apiStepDefinitions")
 @ConfigurationParameter(key = ANSI_COLORS_DISABLED_PROPERTY_NAME, value = "true")
-@ConfigurationParameter(key = FILTER_TAGS_PROPERTY_NAME, value = "@regression")
-public class RegressionRunner {
+@ConfigurationParameter(key = FILTER_TAGS_PROPERTY_NAME, value = "@smoke")
+public class SmokeRunner {
 }
