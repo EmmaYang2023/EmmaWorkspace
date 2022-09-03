aa70d70be3261d2cc2b9780cb8006797feb268ae
Muradil Erkin <miller.muradil@gmail.com>
2022-09-03 11:27:34 -0400

    Added driver factory, organized project, etc

---
+++ src/test/java/uiStepDefinitions/Common.java
@@ -0,0 +1,40 @@
+package uiStepDefinitions;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import utilities.DriverFactory;
+
+public class Common {
+
+	private WebDriver driver = DriverFactory.getInstance();
+
+	@When("user clicks on the {string} navigation link")
+	public void user_clicks_on_the_navigation_link(String navigationLinkText) {
+		driver.findElement(By.linkText(navigationLinkText)).click();
+	}
+
+	@Then("user should be navigated to the {string} page")
+	public void user_should_be_navigated_to_the_page(String pageTitle) {
+		String titleText = driver.findElement(By.cssSelector(".large.text-primary")).getText();
+		assertEquals(pageTitle, titleText);
+		String currentUrl = driver.getCurrentUrl();
+		assertTrue(currentUrl.contains(pageTitle.toLowerCase()));
+	}
+
+	@Then("user should see the success alert")
+	public void user_should_see_the_success_alert() {
+		try {
+			assertTrue(driver.findElement(By.cssSelector(".alert.alert-success")).isDisplayed());
+		} catch (NoSuchElementException e) {
+			assertTrue(false, "The success alert was not found");
+		}
+	}
+
+}
---
+++ src/test/java/uiStepDefinitions/Login.java
@@ -0,0 +1,50 @@
+package uiStepDefinitions;
+
+import io.cucumber.datatable.DataTable;
+import io.cucumber.java.*;
+import io.cucumber.java.en.*;
+import utilities.DriverFactory;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.time.Duration;
+import java.util.Map;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Login {
+
+	private WebDriver driver = DriverFactory.getInstance();
+
+	@Given("user is on the boratech login page")
+	public void navigateToLoginPage() {
+		driver.get("https://boratech.herokuapp.com/login");
+		String titleText = driver.findElement(By.cssSelector(".large.text-primary")).getText();
+		assertEquals("Sign In", titleText);
+	}
+
+	@When("user enters username - {string} and password - {string}")
+	public void user_enters_username_and_password(String username, String password) {
+		driver.findElement(By.xpath("//input[@name='email']")).sendKeys(username);
+		driver.findElement(By.xpath("//input[@name='password']")).sendKeys(password);
+	}
+
+	@When("user clicks on the login button")
+	public void user_clicks_on_the_login_button() throws InterruptedException {
+		driver.findElement(By.xpath("//input[@type='submit']")).click();
+		Thread.sleep(2000);
+	}
+
+	@Given("user is logged in")
+	public void user_is_logged_in(DataTable dataTable) {
+		Map<String, String> credentials = dataTable.asMaps().get(0);
+		driver.get("https://boratech.herokuapp.com/login");
+		driver.findElement(By.xpath("//input[@name='email']")).sendKeys(credentials.get("username"));
+		driver.findElement(By.xpath("//input[@name='password']")).sendKeys(credentials.get("password"));
+		driver.findElement(By.xpath("//input[@type='submit']")).click();
+	}
+
+}
